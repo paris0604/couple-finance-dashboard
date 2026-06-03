f0f5246c3bd9b935a0d81e3ed5a85bce5327e3fe
paris0604 <yoonjiyoung96@gmail.com>
2026-06-03 17:24:29 +0900

    웹앱: Apps Script API_URL 연결

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 /* ★ 여기에 Apps Script 웹앱 URL을 넣으세요.
    apps_script/Code.gs 를 배포하면 나오는 "...exec" 로 끝나는 주소입니다.
    (WEBAPP_DEPLOY.md 3단계 참고) */
-window.API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.API_URL = "https://script.google.com/macros/s/AKfycbwHByuPbza_PRDiNF5QJGbdHghPZlhGT92Afe-qwFhdkLjGf7x7Br7srr6UgaT5dxK7/exec";
